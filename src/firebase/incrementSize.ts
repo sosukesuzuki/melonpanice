682e7cbd3f5fde2c79f6f592ae94151e848ec7df
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-29 00:47:01 +0900

    サイズが120を超えたらもとに戻す

--- src/firebase/incrementSize.ts
+++ src/firebase/incrementSize.ts
@@ -9,6 +9,14 @@ export default async function incrementSize(): Promise<void> {
       x: increment,
       y: increment
     });
+
+    const doc = await melonpanice.get();
+    const { x } = doc.data()!;
+    if (x > 120) {
+      await melonpanice.update({
+        x: 10
+      });
+    }
   } catch (error) {
     throw error;
   }
