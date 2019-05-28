d783732a9a384470df5ae7741d42382fb5b88c0b
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-29 02:30:33 +0900

    bad debug

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "normalize.css";
+import config from "./configuration";
+
+console.log(config.firebase);
 
 ReactDOM.render(<App />, document.querySelector(".root"));
