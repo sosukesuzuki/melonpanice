4a9b88eed3ea351f27e10bfd4a2627e374f00005
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-29 00:39:40 +0900

    画像の大きさの単位をvwで指定

--- src/components/MelonpanIce.tsx
+++ src/components/MelonpanIce.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import { useSize } from "../firebase/hooks/melonpanice";
 import incrementSize from '../firebase/incrementSize';
+
+type ImgProps = {
+  size: number;
+};
+
+const Img = styled.img`
+  width: ${(props: ImgProps) => `${props.size}vw`};
+  height: ${(props: ImgProps) => `${props.size}vw`};
+`;
 
 const MelonpanIce = () => {
   const size = useSize();
 
   if (size === null) return null;
 
-  const { x, y } = size;
+  // よく考えたらXとYを持つ必要なかった。
+  const { x } = size;
   
   return (
-    <img width={x} height={y} src="./assets/logo.svg" onClick={incrementSize} />
+    <Img size={x} src="./assets/logo.svg" onClick={incrementSize} />
   )
 }
 
