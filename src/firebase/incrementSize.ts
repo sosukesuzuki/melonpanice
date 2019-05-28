818cfc3faf5fc18f334f02d5b8f55b3cfac63089
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-29 00:28:32 +0900

    Improve increment size

--- src/firebase/incrementSize.ts
+++ src/firebase/incrementSize.ts
@@ -1,16 +1,13 @@
-import db, { melonpanice } from "./db";
+import firebase from ".";
+import { melonpanice } from "./db";
+
+const increment = firebase.firestore.FieldValue.increment(1);
 
 export default async function incrementSize(): Promise<void> {
   try {
-    await db.runTransaction(async transaction => {
-      const melonpaniceDoc = await transaction.get(melonpanice);
-      if (!melonpaniceDoc.exists) {
-        throw "Document does not exist!";
-      }
-
-      const newX = melonpaniceDoc.data()!.x + 1;
-      const newY = melonpaniceDoc.data()!.y + 1;
-      transaction.update(melonpanice, { x: newX, y: newY });
+    await melonpanice.update({
+      x: increment,
+      y: increment
     });
   } catch (error) {
     throw error;
