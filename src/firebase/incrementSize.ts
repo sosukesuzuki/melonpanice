e58d1d54563292011d02fddf8f2c6b167f81edb0
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-29 13:36:23 +0900

    爆発カウントのインクリメントをトランザクションで処理する

--- src/firebase/incrementSize.ts
+++ src/firebase/incrementSize.ts
@@ -1,8 +1,25 @@
 import firebase from ".";
-import { melonpanice } from "./db";
+import db, { melonpanice } from "./db";
 import debounce from "lodash.debounce";
 
 const increment = firebase.firestore.FieldValue.increment(1);
+
+async function incrementExplosionCount(): Promise<void> {
+  try {
+    await db.runTransaction(async transaction => {
+      const melonpaniceDoc = await transaction.get(melonpanice);
+
+      if (!melonpaniceDoc.exists) {
+        throw "Document does not exist!";
+      }
+
+      const explosion_count = melonpaniceDoc.data()!.explosion_count + 1;
+      transaction.update(melonpanice, { explosion_count });
+    });
+  } catch (error) {
+    throw error;
+  }
+}
 
 export default async function incrementSize(): Promise<void> {
   try {
@@ -16,9 +33,9 @@ export default async function incrementSize(): Promise<void> {
     if (x > 100) {
       debounce(async () => {
         await melonpanice.update({
-          x: 10,
-          explosion_count: increment
+          x: 10
         });
+        await incrementExplosionCount();
       }, 700)();
     }
   } catch (error) {
